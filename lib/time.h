6227c7adb3e1d6982eecb6204abbb79550efa0b3
teilnehmer <jo.hellebusch@web.de>
2019-01-20 11:17:19 +0100

    nothing changed

--- lib/time.h
+++ lib/time.h
@@ -1,9 +1,9 @@
 #ifndef TIME_H_INCLUDED
 #define TIME_H_INCLUDED
 
-int is_leapyear(int year)
-int get_days_for_month(int month, int year)
-int exists_date(int day, int month, int year)
-int day_of_the_year(int day, int month, int year)
+int is_leapyear(int year);
+int get_days_for_month(int month, int year);
+int exists_date(int day, int month, int year);
+int day_of_the_year(int day, int month, int year);
 
 #endif // TIME_H_INCLUDED
